81e931d45a407f47d834bd50b9372465cfc01bb3
mustafa471 <mustafa471@BPC12>
2024-02-22 10:19:20 +0300

    Proje dosyası ekle.

---
+++ haciUygulama/Controllers/sefaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace haciUygulama.Controllers
+{
+    public class sefaController : Controller
+    {
+        public IActionResult Sefa()
+        {
+            return View();
+        }
+        [Route("sefa/Sefa/{id}/{ad}")]
+        public string Liste(int id, string ad)
+        {
+            return $"Parametreden gelen id={id}, ad={ad}";
+        }
+    }
+
+}
